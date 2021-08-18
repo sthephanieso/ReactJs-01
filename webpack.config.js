0a3e530cde35399e147da3b8f306052344779d04
Sthephanie Soares <sthephaniesoares84@gmail.com>
2021-08-18 10:13:19 -0300

    Configurando sass

--- webpack.config.js
+++ webpack.config.js
@@ -38,9 +38,9 @@ module.exports = {
       },
 
       {
-        test: /\.css$/,
+        test: /\.scss$/,
         exclude: /node_modules/, 
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       }
     ]
   }
